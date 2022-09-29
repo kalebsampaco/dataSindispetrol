331fea07aa3029ea2aec0784e773726d2fde6f27
Ubuntu <ubuntu@ip-172-31-10-250.us-east-2.compute.internal>
2022-09-29 16:01:02 +0000

    fixes

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd4c026534bb4e67fc3ab2cfe2558ebf4'),
+    },
+  },
+});
